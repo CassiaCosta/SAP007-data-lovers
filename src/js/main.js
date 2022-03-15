d1ce69a6b9af31ecb33836a1cd422385bc049271
Cássia Costa <cassia.silva93@hotmail.com>
2022-03-15 12:44:14 -0300

    Transição do hover dos cards

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import { alphabeticalFilter, calculos, rarityFilter, regionFilter, searchName, sizeFilter, typeFilter } from './data.js';
 import data from '../data/pokemon/pokemon.js';
 
+const allPokemons = data.pokemon;
+
 let selectType = document.getElementById('typeFilter');
 let selectRarity = document.getElementById('rarityFilter');
 let selectRegion = document.getElementById('regionFilter');
@@ -10,98 +12,159 @@ let clearButton = document.getElementById('clearButton');
 let calculationBar = document.getElementById('calculation');
 let buttonTop = document.getElementById('buttonTop');
 
+function translate(type) {
+  let translatedType = "";
+  switch (type) {
+    case "water":
+      translatedType = "Água";
+      break;
+    case "dragon":
+      translatedType = "Dragão";
+      break;
+    case "electric":
+      translatedType = "Elétrico";
+      break;
+    case "ghost":
+      translatedType = "Fantasma";
+      break;
+    case "fire":
+      translatedType = "Fogo";
+      break;
+    case "ice":
+      translatedType = "Gelo";
+      break;
+    case "bug":
+      translatedType = "Inseto";
+      break;
+    case "fighting":
+      translatedType = "Lutador";
+      break;
+    case "normal":
+      translatedType = "Normal";
+      break;
+    case "rock":
+      translatedType = "Pedra";
+      break;
+    case "grass":
+      translatedType = "Planta";
+      break;
+    case "psychic":
+      translatedType = "Psíquico";
+      break;
+    case "ground":
+      translatedType = "Terra";
+      break;
+    case "poison":
+      translatedType = "Venenoso";
+      break;
+    case "flying":
+      translatedType = "Voador";
+      break;
+  }
+  return translatedType;
+}
+
 function showPokemon(data) {
-    document.getElementById('pokemonList').innerHTML = data.map((item) => `
-    <button aria-label="clique para mais infomacoes" class="cards" onclick="(this.classList.toggle('active'))">
-        <section class="front-cards" id="frontCards">
-            <p class="numberPokemon">${item.num}</p>
-            <picture>
-                <img class="image-card" src="${item.img}" alt="imagem do Pokémon">
-            </picture>
-            <div class="info-cards">
-                <p class="namePokemon">${item.name}</p>
-                <p class="typePokemon"> <b>Tipo:</b> ${item.type}</p>
-                <p class="regionPokemon"> <b>Região:</b> ${item.generation["name"]}</p>
-            </div>
-        </section>
-        <section class="back-cards" id="backCards">
-            <p class="heightPokemon"> <b> Altura:</b> ${item.size["height"]}</p>
-            <p class="weightPokemon"> <b> Peso: </b> ${item.size["weight"]}</p>
-            <p class="rarityPokemon"> <b>Raridade:</b> ${item["pokemon-rarity"]}</p>
-            <p class="specialAttackPokemon"> <b>Ataque especial:</b> <br> ${item["special-attack"][0].name}</p>
-        </section>
-    </button>
+  const allCards = data.map((item) => `
+    <div class="allCards">
+        <div class="hoverCards">
+            <section class="front-cards" id="frontCards">
+                <p class="numberPokemon">${item.num}</p>
+                <picture>
+                    <img class="image-card" src="${item.img}" alt="imagem do Pokémon" loading = "lazy">
+                </picture>
+                <div class="info-cards">
+                    <p class="namePokemon">${item.name}</p>
+                    <p class="typePokemon"> <b>Tipo:</b> ${translate(item.type)}</p>
+                    <p class="regionPokemon"> <b>Região:</b> ${item.generation["name"]}</p>
+                </div>
+            </section>
+            <section class="back-cards" id="backCards">
+                <p class="heightPokemon"> <b> Altura:</b> ${item.size["height"]}</p>
+                <p class="weightPokemon"> <b> Peso: </b> ${item.size["weight"]}</p>
+                <p class="rarityPokemon"> <b>Raridade:</b> ${item["pokemon-rarity"]}</p>
+                <p class="specialAttackPokemon"> <b>Ataque especial:</b> <br> ${item["special-attack"][0].name}</p>
+            </section>
+        </div>
+    </div>
     `).join('')
+    document.getElementById('pokemonList').innerHTML = allCards;
 }
 
 function searchByType(e) {
-    const resultEspecie = typeFilter(data.pokemon, e.target.value)
-    calculationBar.innerHTML = `Este tipo de pokémon representa ${calculos(data.pokemon.length, resultEspecie.length)}% 
+  const resultEspecie = typeFilter(allPokemons, e.target.value)
+  calculationBar.innerHTML = `Este tipo de pokémon representa ${calculos(allPokemons.length, resultEspecie.length)}% 
         do total`
-    return showPokemon(resultEspecie)
+  return showPokemon(resultEspecie)
 }
 
 function searchByRarity(e) {
-    const resultRarity = rarityFilter(data.pokemon, e.target.value)
-    calculationBar.innerHTML = `Esta raridade de pokémon representa ${calculos(data.pokemon.length, resultRarity.length)}% 
+  const resultRarity = rarityFilter(allPokemons, e.target.value)
+  calculationBar.innerHTML = `Esta raridade de pokémon representa ${calculos(allPokemons.length, resultRarity.length)}% 
         do total`
-    return showPokemon(resultRarity)
+  return showPokemon(resultRarity)
 }
 
 function searchByRegion(e) {
-    const resultRegion = regionFilter(data.pokemon, e.target.value)
-    calculationBar.innerHTML = `${calculos(data.pokemon.length, resultRegion.length)}% dos Pokémons pertencem a esta região`
-    return showPokemon(resultRegion)
+  const resultRegion = regionFilter(allPokemons, e.target.value)
+  calculationBar.innerHTML = `${calculos(allPokemons.length, resultRegion.length)}% dos Pokémons pertencem a esta região`
+  return showPokemon(resultRegion)
 }
 
 function searchByOrderAlphabetical() {
-    if(selectOrder.value == "a-z") {
-        calculationBar.innerHTML = `Pokémons ordenados de A à Z`
-        return showPokemon(alphabeticalFilter(data.pokemon, selectOrder.value))
-    } else if(selectOrder.value == "z-a") {
-        calculationBar.innerHTML = `Pokémons ordenados de Z à A`
-        return showPokemon(alphabeticalFilter(data.pokemon, selectOrder.value))
-    }
+  if (selectOrder.value == "a-z") {
+    calculationBar.innerHTML = `Pokémons ordenados de A à Z`
+    return showPokemon(alphabeticalFilter(allPokemons, selectOrder.value))
+  } else if (selectOrder.value == "z-a") {
+    calculationBar.innerHTML = `Pokémons ordenados de Z à A`
+    return showPokemon(alphabeticalFilter(allPokemons, selectOrder.value))
+  }
 }
 
 function searchByHeightOrder() {
-    if(selectOrder.value == "shortHigh") {
-        calculationBar.innerHTML = `Pokémons ordenados do mais baixo para o mais alto`
-        return showPokemon(sizeFilter(data.pokemon, "height", selectOrder.value))
-    } else if(selectOrder.value == "highShort") {
-        calculationBar.innerHTML = `Pokémons ordenados do mais alto para o mais baixo`
-        return showPokemon(sizeFilter(data.pokemon, "height", selectOrder.value))
-    }
+  if (selectOrder.value == "shortHigh") {
+    calculationBar.innerHTML = `Pokémons ordenados do mais baixo para o mais alto`
+    return showPokemon(sizeFilter(allPokemons, "height", selectOrder.value))
+  } else if (selectOrder.value == "highShort") {
+    calculationBar.innerHTML = `Pokémons ordenados do mais alto para o mais baixo`
+    return showPokemon(sizeFilter(allPokemons, "height", selectOrder.value))
+  }
 }
 
 function searchByWeightOrder() {
-    if(selectOrder.value == "lightHeavy") {
-        calculationBar.innerHTML = `Pokémons ordenados do mais leve para o mais pesado`
-        return showPokemon(sizeFilter(data.pokemon, "weight", selectOrder.value))
-    } else if(selectOrder.value == "heavyLight") {
-        calculationBar.innerHTML = `Pokémons ordenados do mais pesado para o mais leve`
-        return showPokemon(sizeFilter(data.pokemon, "weight", selectOrder.value))
-    }
+  if (selectOrder.value == "lightHeavy") {
+    calculationBar.innerHTML = `Pokémons ordenados do mais leve para o mais pesado`
+    return showPokemon(sizeFilter(allPokemons, "weight", selectOrder.value))
+  } else if (selectOrder.value == "heavyLight") {
+    calculationBar.innerHTML = `Pokémons ordenados do mais pesado para o mais leve`
+    return showPokemon(sizeFilter(allPokemons, "weight", selectOrder.value))
+  }
 }
 
 function searchByName() {
-    if(inputName.value !== ""){
-        calculationBar.innerHTML = `Busca por nome...`
-    } else if(inputName.value == "") {
-        calculationBar.innerHTML = `Você está vendo todos os Pokémons!`
-    }
-    return showPokemon(searchName(data.pokemon, inputName.value))
+  if (inputName.value !== "") {
+    calculationBar.innerHTML = `Busca por nome...`
+  } else if (inputName.value == "") {
+    calculationBar.innerHTML = `Você está vendo todos os Pokémons!`
+  }
+  return showPokemon(searchName(allPokemons, inputName.value.toLowerCase()))
 }
 
 function cleanFilters() {
-    window.location.reload()
+  showPokemon(allPokemons);
+  selectType.value = "";
+  selectRarity.value = "";
+  selectRegion.value = "";
+  selectOrder.value = "";
+  inputName.value = "";
+  calculationBar.innerHTML = `Você está vendo todos os Pokémons!`
 }
 
 function scrollToTop() {
-    window.scrollTo({top: 0, behavior: 'smooth'});
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
-showPokemon(data.pokemon);
+showPokemon(allPokemons);
 selectType.addEventListener('change', searchByType);
 selectRarity.addEventListener('change', searchByRarity);
 selectRegion.addEventListener('change', searchByRegion);
